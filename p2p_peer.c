a37eab8f7a6fd8318a1c10a580c3cc60f0876265
jenjen <perarduaadastra3@gmail.com>
2021-05-18 22:16:35 +0900

    basic framework for sending alive message

--- p2p_peer.c
+++ p2p_peer.c
@@ -7,15 +7,17 @@
 #include <string.h>
 #define BUF_SIZE 100
 
+int get_nlist(int sockfd, struct sockaddr_in *neighbor_list);
+
 int main(int argc, char const *argv[])
 {
-    int sock = 0, valread;
     int serv_sd, client_sd;
     int fd_max;
-    int activity, str_len;
-    int client_addr_size;
-    struct sockaddr_in serv_addr, client_addr;
-    struct sockaddr_in neighborList[3];
+    int fd_num, str_len;
+    int clnt_adr_sz;
+    int neighbor_size;
+    struct sockaddr_in serv_addr, client_adr;
+    struct sockaddr_in neighbor_list[3];
 
     char *alive_message = "ALIVE";
     char buffer[BUF_SIZE];
@@ -30,25 +32,29 @@ int main(int argc, char const *argv[])
         exit(1);
     }
 
-    sock = socket(PF_INET, SOCK_STREAM, 0);
+    //소켓 생성
+    serv_sd = socket(PF_INET, SOCK_STREAM, 0);
 
     memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
     serv_addr.sin_port = htons(atoi(argv[2]));
 
-    //이부분 야매로 함 바꿀것
+    // connect(sd, (struct sockaddr*)&serv_adr, sizeof(serv_adr));
+
     bind(serv_sd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
     listen(serv_sd, 4);
 
-    //INITIALIZE FD
+    // fd table 초기화
     FD_ZERO(&fds);
     FD_SET(serv_sd, &fds);
     fd_max = serv_sd;
+
+    //TODO: timeout 설정
+
     while (1)
     {
-
-        if ((activity = select(fd_max + 1, &fds, NULL, NULL, NULL)) == -1)
+        if ((fd_num = select(fd_max + 1, &fds, NULL, NULL, NULL)) == -1)
         {
             printf("select error");
             break;
@@ -58,44 +64,89 @@ int main(int argc, char const *argv[])
         {
             if (FD_ISSET(i, &fds))
             {
-                // 새로오는 걸 받는 거니까
-                // tracker에서 오는 거면 neighbor list를 업데이트
-
                 if (i == serv_sd)
                 {
-                    client_addr_size = sizeof(client_addr);
-                    client_sd = accept(serv_sd, (struct sockaddr *)&client_addr, &client_addr_size);
+                    clnt_adr_sz = sizeof(client_adr);
+                    client_sd = accept(serv_sd, (struct sockaddr *)&client_adr, &clnt_adr_sz);
 
-                    if ((str_len = recv(client_sd, buffer, BUF_SIZE, 0)) == -1)
+                    FD_SET(client_sd, &fds);
+                    if(fd_max<client_sd)
+                        fd_max = client_sd;
+                }
+                else {
+                    //read message
+                    if ((str_len = recv(i, buffer, BUF_SIZE, 0)) == -1)
                     {
                         error_handling("ERROR: receiving message");
                     }
                     strcpy(message, buffer);
                     message_type = strtok(message, '/');
+                    
+                    // tracker가 neighbor list를 보내줌 : type == NLIST ?
+                    if (strcmp(message_type,"NLIST")==0)
+                    {
+                        // parse message
+                        neighbor_size = get_nlist(i, neighbor_list);
+
+                        // update fd table (reset)
+                        for (int j=0;j<fd_max;j++){
+                            if(j!=0 && j!=serv_sd){
+                                FD_CLR(j,&fds);
+                            }
+                        }
+                        // update neighbor address list
+
+
+                        // request chunk list to neighbors
+                        
+                    }
+                    // neighbor가 보유 chunk 목록을 요청 : type == CRQST ?
+                    else if (strcmp(message_type,"LRQST")==0) {
+                        // 소유 chunk offset list를 serialize
+                        
+                        // serialized된 list를 neighbor에 보내기
+                    }
+                    // neighbor가 특정 chunk 데이터를 요청 : type == DRQST ?
+                    else if (strcmp(message_type, "DRQST")==0){
+                        
+                    }
+                    // neighbor가 chunk 데이터 목록을 응답해옴 : type == LRESP ?
+                    else if (strcmp(message_type, "LRESP")==0){
+                        
+                    }
+                    // neighbor가 요청한 데이터를 응답해옴 : type == DRESP ?
+                    else if (strcmp(message_type, "DRESP")==0){
+                        
+                    }
+                    else {
+                        // invalid message
+                        error_handling("ERROR: survival from peer");
+                    }
                 }
             }
         }
-
-        send(sock, alive_message, strlen(alive_message), 0);
-        printf("ALIVE message sent\n");
-
-        //get updated neighbor list
-
-        //이웃들한테 chunk list request 메시지 보내기
-        for (int i = 0; i < 3; i++)
-        {
-            //send LISTREQ
-
-            //RECEIVE
-        }
-
-        sleep(1000);
     }
 
-    close(sock);
-    // valread = read( sock , buffer, 1024);
-    // printf("%s\n",buffer );
+    close(serv_sd);
     return 0;
+}
+
+int get_nlist(int sockfd, struct sockaddr_in *neighbor_list) {
+    char message[BUF_SIZE];
+    char * buf;
+    int neighbor_size;
+    int n;
+
+    recv(sockfd, message, BUF_SIZE, 0);
+    buf = strtok(message, "/");
+    neighbor_size = atoi(buf);
+    for (n = 0; n < neighbor_size; n++) {
+        buf = strtok(NULL, "/");
+        printf("neighbor[%d]: %s\n", n, buf);
+        inet_pton(AF_INET, buf, &(neighbor_list[n].sin_addr));
+    }
+
+    return neighbor_size;
 }
 
 void error_handling(char *message)
--- p2p_tracker.c
+++ p2p_tracker.c
@@ -4,29 +4,31 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <time.h>
 
 #define BUF_SIZE 30
 #define MAX_CLIENT 10
+#define MAX_NEIGHBOR 3
 #define TIME_OUT 1000
 void error_handling(char *message);
 
 int main(int argc, char *argv[])
 {
-
-    //각 소켓의 번호
     int serv_sd, client_sd;
     int fd_max;
-    int activity, str_len;
+    int fd_num, str_len;
+    int neighbor_size;
     char buf[BUF_SIZE];
     char message[BUF_SIZE];
     char *nlist_message = "NLIST";
 
-    struct sockaddr_in serv_adr, clnt_adr[MAX_CLIENT], client_adr;
+    struct sockaddr_in serv_adr, peer_list[MAX_CLIENT], neighbors_to_send[MAX_NEIGHBOR], client_adr;
     struct timeval timeout;
+
 
     socklen_t clnt_adr_sz;
 
-    fd_set readfds;
+    fd_set fds;
 
     if (argc < 2)
     {
@@ -43,12 +45,13 @@ int main(int argc, char *argv[])
     serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_adr.sin_port = htons(atoi(argv[1]));
 
+    // welcoming socket (최대 10개까지 listen)
     bind(serv_sd, (struct sockaddr *)&serv_adr, sizeof(serv_adr));
     listen(serv_sd, 10);
 
-    FD_ZERO(&readfds);
-    //server socket fd에 등록
-    FD_SET(serv_sd, &readfds);
+    // fd table 초기화
+    FD_ZERO(&fds);
+    FD_SET(serv_sd, &fds);
     fd_max = serv_sd;
 
     timeout.tv_sec = 5;
@@ -56,53 +59,38 @@ int main(int argc, char *argv[])
 
     while (1)
     {
-        if ((activity = select(fd_max + 1, &readfds, NULL, NULL, fd_max == serv_sd ? NULL : &timeout)) == -1)
+        // select: fd table 에 등록된 애들 중 event 생긴 socket 수 return
+        if ((fd_num = select(fd_max + 1, &fds, NULL, NULL, fd_max == serv_sd ? NULL : &timeout)) == -1)
         {
             printf("select error");
             break;
         }
-
-        else if (activity == 0)
+        else if (fd_num == 0) //timeout
         {
             printf("No peers requesting!\n");
         }
 
+        // fd table 돌면서 event 생겼는지 확인
         for (int i = 0; i < fd_max + 1; i++)
         {
-            if (FD_ISSET(i, &readfds))
+            if (FD_ISSET(i, &fds))
             {
-                //server socket에서 일어난 일
+                //welcoming socket에서 생긴 event
                 if (i == serv_sd)
                 {
-                    clnt_adr_sz = sizeof(clnt_adr[fd_max]);
-                    //incoming peer일때 address 받아옴,,,??
+                    /*
+                    새 connection 을 받아들여, fd table 에 자리 내줌
+                    */
+                    clnt_adr_sz = sizeof(peer_list[fd_max]);
                     client_sd = accept(serv_sd, (struct sockaddr *)&client_adr, &clnt_adr_sz);
-                    clnt_adr[client_sd] = client_adr;
-                    //save client address
+                    inet_ntop(AF_INET, &(client_adr.sin_addr), buf, INET_ADDRSTRLEN);
+                    
+                    printf("Welcome! %s\n", buf);
+                    //save client address to peer list
+                    peer_list[client_sd] = client_adr;
 
-                    //send neighbor list
-                    if ((str_len = recv(client_sd, buf, BUF_SIZE, 0)) == -1)
-                    {
-                        error_handling("ERROR: receiving message from peers");
-                    }
-                    strcpy(message, buf);
-
-                    printf("%s\n", message);
-                    if (strcmp("ALIVE", message) == 0)
-                    {
-                        // NLIST/(수)/(IP1)/(IP2)/...
-                        // 새로운 neightbor list 보내주기
-                        send(client_sd, nlist_message, strlen(nlist_message), 0);
-                        // 보내야 할 neighbor 찾기
-                        inet_ntop(AF_INET, &(serv_adr.sin_addr), message, INET_ADDRSTRLEN);
-                        send(client_sd, nlist_message, strlen(nlist_message), 0);
-                    }
-                    else
-                    {
-                        error_handling("ERROR: survival from peer");
-                    }
-
-                    FD_SET(client_sd, &readfds);
+                    //update fd table
+                    FD_SET(client_sd, &fds);
                     if (fd_max < client_sd)
                     {
                         fd_max = client_sd;
@@ -110,27 +98,47 @@ int main(int argc, char *argv[])
                 }
                 else
                 {
-                    //event occured to one of the clients
-                    str_len = read(i, buf, BUF_SIZE);
-                    if (str_len == 0)
+                    // read message
+                    if ((str_len = recv(i, message, BUF_SIZE, 0)) == -1)
                     {
-                        //close reqest??
+                        error_handling("ERROR: receiving message from peers");
+                    }
+
+                    if (strcmp("ALIVE", message) == 0)
+                    {
+                        // 새 nlist 뽑아주기
+                        // serialize message
+                        // 보냄 : NLIST/(수)/(IP1)/(IP2)/...
+                        send(client_sd, nlist_message, strlen(nlist_message), 0);
+                        // 보내야 할 neighbor 찾기
+                        neighbor_size = get_random_neighbors(i,client_sd, &neighbors_to_send);
+
+                        sprintf(message, "%d/", neighbor_size);
+                        for (int n = 0; n < neighbor_size; n++) {
+                            inet_ntop(AF_INET, &(neighbors_to_send[n].sin_addr), buf, INET_ADDRSTRLEN);
+
+                            printf("%s\n",buf);
+
+                            strcat(message, buf);
+                            strcat(message, n == (neighbor_size - 1) ? "" : "/");
+                        }
+                        send(client_sd, message, strlen(message), 0);
                     }
                     else
                     {
-                        //생존 신고 보내왔음
-                        printf("%s\n", buf);
+                        // invalid message
+                        error_handling("ERROR: survival from peer");
                     }
                 }
             }
             else
             {
-                if (i != serv_sd)
+                //welcoming socket 이 아닌데?? alive message 를 안 보낸다?? -> 죽은 것으로 판단
+                if (i != serv_sd) 
                 {
-                    //죽은것으로 판단
                     printf("Peer %d is dead\n", i);
                     //update registered list
-                    unregister_neighbor(clnt_adr, &readfds, i);
+                    unregister_neighbor(peer_list, &fds, i);
                 }
             }
         }
@@ -140,10 +148,49 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-// int send_neighbor_list(struct sockaddr_in peer, struct sockaddr_in registered, fd_set)
-// {
+// 배열을 섞는 함수 
+void shuffle(int *arr, int num) { 
+    srand(time(NULL)); 
+    int temp; 
+    int rn; 
+    for (int i=0; i < num-1; i++) {
+         rn = rand() % (num - i) + i; // i 부터 num-1 사이에 임의의 정수 생성 
+         temp = arr[i]; 
+         arr[i] = arr[rn]; 
+         arr[rn] = temp; 
+    } 
+}
 
-// }
+int get_random_neighbors(int req_fd, struct sockaddr_in* peers, struct sockaddr_in* neighbors){
+    int candidate_idx[MAX_CLIENT];
+    int idx=-1;
+    struct sockaddr_in invalid_addr;
+    memset(&invalid_addr,0,sizeof(invalid_addr));
+    
+    for(int i=0;i<MAX_CLIENT;i++){
+        if(i==req_fd)
+            continue;
+        if(memcmp(&peers[i],&invalid_addr,sizeof(invalid_addr))==0)
+            continue;
+        candidate_idx[++idx] = i;
+    }
+    //randomize candidate list
+    shuffle(candidate_idx,idx+1);
+    
+    printf("Debugging... print randomized candidates idx\n");
+    for(int i=0;i<idx;i++){
+        printf("%d ",candidate_idx[i]);
+    }
+    printf("\nReturning total %d neighbors\n",idx);
+
+    //set first N elements in <neighbors>
+    for(int i=0;i<(idx+1>MAX_NEIGHBOR)?MAX_NEIGHBOR:idx+1;i++){
+        neighbors[i] = peers[candidate_idx[i]];
+    }
+
+    return idx;
+}
+
 
 int unregister_neighbor(struct sockaddr_in *peers, fd_set *set, int fd)
 {
